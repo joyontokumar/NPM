7f2251ce129e035bda453098ca667629d3a61ee3
joyonto kumar roy <36919219+joyontokumar@users.noreply.github.com>
2018-09-29 23:41:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*==============================================
+	Author : Joyonto Kumar Roy
+	Author URI: www.hamaronline.com
+==============================================*/
+
+$(window).on('load', function(){
+
+	$('.preloader').fadeOut(5000);
+
+});
